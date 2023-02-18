9a828ca26a545f712478b20b3b89e585d421bb56
Faisal6274 <faisaldevbhaluka@gmail.com>
2023-02-18 22:04:45 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+          "primary": "#01051e",
+          "orange": "#E39929",
+          "gray": "#F3F5F8",
+          "black": "#2E2F33",
+          "black-dark": "#283842",
+          "secondary": "#ff7d3d",
+          "btn-color": "#00aeef",
+          "btn-2": "#e79800",
+          "p-color": "#777",
+      }
+    },
+   container: {
+     center: true,
+     padding: {
+      DEFAULT: '1rem',
+      sm: '2rem',
+      lg: '4rem',
+      xl: '5rem',
+      '2xl': '6rem',
+    },
+   }
+  },
+  plugins: [],
+}
